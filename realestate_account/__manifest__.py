59a5618c864d1b4a6b001f09051d5f02baf050b2
devil1289humbleAA <124971664+devil1289humbleAA@users.noreply.github.com>
2023-11-14 09:25:11 -0500

    Add files via upload

---
+++ realestate_account/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Real-Estate Accounting",
+    'version': "14.0.1.0.0",
+    'sequence': '-100',
+    'author': "Sameem Arshad",
+    'description': """Real-Estate Management for Axiom World""",
+    'category': "Real Estate",
+    'depends': ['realestate', 'account'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application':  True,
+}

8add2a5257406c6dee451867d3327628f3f0c0f2
devil1289humbleAA <124971664+devil1289humbleAA@users.noreply.github.com>
2023-11-15 07:41:54 -0500

    Delete realestate directory

--- realestate/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import realestate_property
-from . import realestate_type
-from . import realestate_offer
-from . import realestate_tags
-from . import res_users
--- realestate/models/realestate_tags.py
+++
@@ -1,9 +0,0 @@
-
-
-from odoo import models, fields
-
-class EstatePropertyTag(models.Model):
-    _name = "estate.property.tag"
-    _description = "Real Estate Property Tag"
-
-    name = fields.Char(string="Tag", required=True)
